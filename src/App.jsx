feb7e1d5da422e9e157dbfde3266c02a23e7689b
Bill Stevens <96736755+stevensaero@users.noreply.github.com>
2026-05-19 12:52:34 -0600

    first commit - hours tool

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import MyAwesomeComponent from './components/hours-tool_v0.2.3.jsx';   // ← CHANGE THIS
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My React App on GitHub Pages</h1>
+      <MyAwesomeComponent />
+    </div>
+  );
+}
+
+export default App;
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: '/orthodox-hours/',   // we'll use this later for GitHub Pages
+})
